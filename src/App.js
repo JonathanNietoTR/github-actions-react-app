bdd784425ad0e43ace12aac820c953a56ced321b
Jonathan Nieto <jonathan.nietodavila@thomsonreuters.com>
2020-05-20 14:20:37 -0500

    Fix tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn  & Github Actions
+          Learn React & Github Actions
         </a>
       </header>
     </div>
